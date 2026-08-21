b17e781af81f79e2b5fb87ce9b466e6c94a60741
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-08-21 10:37:02 +0200

    Established layout of GameBoard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,19 +2,24 @@ import { useEffect } from "react";
 import GameCard from "./GameCard";
 
 const pokemon = [
-    pikachu,
-    charizard,
-    mewtwo,
-    gengar,
-    greninja,
-    snorlax,
+    "pikachu",
+    "charizard",
+    "mewtwo",
+    "gengar",
+    "greninja",
+    "snorlax",
 ]
 
 function GameBoard() {
 
     return (
         <div>
-            
+            {pokemon.map((name) => {
+                return <GameCard
+                    name={name}
+                    key={name}
+                />
+            })}
         </div>
     )
 }
